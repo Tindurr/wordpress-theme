d15e226d17336328d4f1bf484531a5819c890850
Tindurr <42875761+Tindurr@users.noreply.github.com>
2020-01-22 12:08:50 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -15,11 +15,11 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Whoopsie. That was not supposed to happen.', 'weaver' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( "Whoopsie. That was not supposed to happen.", "weaver" ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It may be a 404, but have the teapot from 418 anyways. We do not get to use it much.', 'weaver' ); ?></p>
+					<p><?php esc_html_e( "It may be a 404, but have the teapot from 418 anyways. We do not get to use it much.", "weaver" ); ?></p>
 					<img src="wp-content\themes\wordpress-theme\images\Utah_teapot_simple_2.png" alt="a Utah Teapot." height="300" width="300">
 
 					<?php
