cda925d2f1a4125a54f16a4b9a9b86adf8d9b41a
Tindurr <42875761+Tindurr@users.noreply.github.com>
2020-01-30 07:32:53 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -159,3 +159,20 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_theme_support( 'weaver-custom-header' );
+
+function weaver_custom_header_setup() {
+    $defaults = array(
+        // Default Header Image to display
+        'default-image'         => get_template_directory_uri() . '/images/headers/default.jpg',
+        // Display the header text along with the image
+        'header-text'           => false,
+        // Header text color default
+        'default-text-color'        => '000',
+        // Header image width (in pixels)
+        'width'             => 1000,
+        // Header image height (in pixels)
+        'height'            => 198,
+        // Header image random rotation default
+
+
